d0e71ce04ff238b2c356069965017803b2cc68de
Mikhail.Kondratyev <mikekondratyev@gmail.com>
2023-04-04 20:10:58 +0300

    ok

--- src/main/java/Lesson1/Rectangle/Rectangle.java
+++ src/main/java/Lesson1/Rectangle/Rectangle.java
@@ -1,16 +1,40 @@
 package Lesson1.Rectangle;
 
 public class Rectangle {
+private Point se;
+private Point nw;
 
+    public Rectangle(Point se, Point nw) {
+        this.se = se;
+        this.nw = nw;
+    }
 
+    public Point getSe() {
+        return se;
+    }
+
+    public void setSe(Point se) {
+        this.se = se;
+    }
+
+    public Point getNw() {
+        return nw;
+    }
+
+    public void setNw(Point nw) {
+        this.nw = nw;
+    }
+
+    int area() {
+        int sizeX = se.getX() - nw.getX();
+        int sizeY = se.getY() - nw.getY();
+        return sizeY*sizeX;
+
+    }
 }
 
-//    int se = // добавьте свойства se и nw - юго восточная точка и северозападная
-//    Point se;
-//    Point nw;
-//    // добавьте конструктор
-//    // геттеры и сеттеры
-//    double area()
+
+
 //    {
 //        // вернуть площадь
 //        return 0;
--- src/main/java/Lesson1/Rectangle/Tester.java
+++ src/main/java/Lesson1/Rectangle/Tester.java
@@ -4,7 +4,7 @@ public class Tester {
     public static void main(String[] args) {
         Point point1 = new Point(10,10);
         Point point2 = new Point(30,30);
-        double area = (point2.getX()-point1.getX())*(point2.getY()-point1.getY());
-        System.out.println(area);
+        Rectangle r= new Rectangle(point2, point1);
+        System.out.println(r.area());
     }
 }
